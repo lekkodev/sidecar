e8e8341eb1739f4e94b71679b1d1e6cb19e1fb97
Shubhit Singh <shubhit@lekko.dev>
2022-12-08 22:12:39 -0800

    build GetRepositoryContentsResponse

--- src/fallback.rs
+++ src/fallback.rs
@@ -1,8 +1,13 @@
 use std::fs::{read, read_dir};
 
+use prost::Message;
+use sha1::Digest;
 use tonic::Status;
 
-use crate::gen::lekko::backend::v1beta1::{RepositoryKey, GetRepositoryContentsResponse, Namespace};
+use crate::gen::lekko::{
+    backend::v1beta1::{Feature, GetRepositoryContentsResponse, Namespace, RepositoryKey},
+    feature,
+};
 
 pub struct Fallback {
     // If none, fallback behavior is not enabled.
@@ -16,7 +21,11 @@ impl Fallback {
     pub fn enabled(&self) -> bool {
         self.repo_path.is_some()
     }
-    pub fn load(&self, repo_key: RepositoryKey, namespaces: &[String]) -> Result<GetRepositoryContentsResponse, Status> {
+    pub fn load(
+        &self,
+        repo_key: RepositoryKey,
+        namespaces: &[String],
+    ) -> Result<GetRepositoryContentsResponse, Status> {
         println!(
             "Fallback: load {:?} {:?} {:?}",
             self.repo_path, repo_key, namespaces
@@ -25,10 +34,12 @@ impl Fallback {
         for namespace in namespaces {
             ns_results.push(self.load_namespace(namespace)?);
         }
-        Ok(GetRepositoryContentsResponse {
-            commit_sha: "".to_string(),
+        let resp = GetRepositoryContentsResponse {
+            commit_sha: "".to_string(), // TODO: get commit sha from git repo
             namespaces: ns_results,
-        })
+        };
+        println!("git-sync commit sha {:?}", resp.commit_sha);
+        Ok(resp)
     }
 
     fn load_namespace(&self, namespace: &str) -> Result<Namespace, Status> {
@@ -49,46 +60,81 @@ impl Fallback {
                         e
                     )))
                 }
-                Ok(p) => {
-                    println!("Name: {}", p.path().display());
-                    match p.file_type() {
-                        Err(e) => {
-                            return Err(Status::invalid_argument(format!(
-                                "failed to get file type {:?}",
-                                e
-                            )))
-                        }
-                        Ok(ft) => {
-                            if ft.is_file() {
-                                match read(p.path()) {
-                                    Err(e) => {
-                                        return Err(Status::internal(format!(
-                                            "failed to read path: {:?}",
-                                            e
-                                        )))
-                                    }
-                                    Ok(bytes) => {
-                                        println!(
-                                            "file name {:?} has {:?} bytes",
-                                            p.file_name(),
-                                            bytes.len()
-                                        );
-                                        let filename = match p.file_name().to_str() {
-                                            Some(file) => file,
-                                            None => return Err(Status::internal("file name empty")),
-                                        };
-                                        let feature_name = 
-                                        let s = p.file_name().to_str().unwrap().split_once(".");
-                                    }
+                Ok(p) => match p.file_type() {
+                    Err(e) => {
+                        return Err(Status::invalid_argument(format!(
+                            "failed to get file type {:?}",
+                            e
+                        )))
+                    }
+                    Ok(ft) => {
+                        if ft.is_file() {
+                            match read(p.path()) {
+                                Err(e) => {
+                                    return Err(Status::internal(format!(
+                                        "failed to read path: {:?}",
+                                        e
+                                    )))
                                 }
-                            } else {
-                                continue;
+                                Ok(bytes) => {
+                                    let filename = p.file_name();
+                                    let filename = match filename.to_str() {
+                                        Some(file) => file,
+                                        None => return Err(Status::internal("file name empty")),
+                                    };
+                                    let feature_name = match filename.strip_suffix(".proto.bin") {
+                                        Some(a) => a,
+                                        None => {
+                                            println!("malformed filename in gen/proto, skipping");
+                                            continue;
+                                        }
+                                    };
+                                    let sha = self.blob_sha(bytes.as_ref())?;
+                                    features.push(Feature {
+                                        name: String::from(feature_name),
+                                        sha: sha.to_owned(),
+                                        feature: match feature::v1beta1::Feature::decode(
+                                            bytes.as_ref(),
+                                        ) {
+                                            Ok(d) => Some(d),
+                                            Err(e) => {
+                                                println!("decode error! {:?}", e);
+                                                return Err(Status::internal(format!(
+                                                    "decode feature from git-sync: {:?}",
+                                                    e
+                                                )));
+                                            }
+                                        },
+                                    });
+                                    println!(
+                                        "{:} [{:?} bytes]: sha {:?}",
+                                        feature_name,
+                                        bytes.len(),
+                                        sha,
+                                    );
+                                }
                             }
+                        } else {
+                            continue;
                         }
                     }
-                }
+                },
             };
         }
-        Ok(Namespace { name: namespace.to_owned(), features })
+        Ok(Namespace {
+            name: namespace.to_owned(),
+            features,
+        })
+    }
+
+    // Calculates the git sha of a blob. Underlying algorithm uses
+    // sha-1 with some prefixed bytes. see
+    // https://stackoverflow.com/a/24283352/1849010
+    fn blob_sha(&self, content: &[u8]) -> Result<String, Status> {
+        let mut hasher = sha1::Sha1::new();
+        let prefix = format!("blob {:}\0", content.len());
+        hasher.update(prefix.as_bytes());
+        hasher.update(content);
+        Ok(format!("{:x}", hasher.finalize()))
     }
 }
