858194de06474b87f0d617678af400100db064f3
Shubhit Singh <shubhit@lekko.dev>
2023-02-09 18:16:49 -0800

    release

--- release.sh
+++ release.sh
@@ -8,10 +8,12 @@ echo What version would you like to tag this release? e.g. v0.1.0:
 read version
 
 echo Tagging version $version
-git tag -a $version -m $version
+git tag -a $version -m $version -f
 git push origin --tags
 
 echo Pushing $version to $registry/$repository 
 docker build -t $registry/$repository:$version -f Dockerfile.sidecar --platform=linux/amd64 .
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $registry
 docker push $registry/$repository:$version
+
+echo Released $version. View the release here: https://gallery.ecr.aws/g0r8j2n2/lekko/sidecar
