58ff60b9586f24e912ee600e8729a9f52ce2b284
Shubhit Singh <shubhit@lekko.dev>
2023-02-13 12:41:30 -0800

    check for login creds first

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@
 registry=public.ecr.aws/g0r8j2n2
 repository=lekko/sidecar
 
+# First, check for credentials so we can later proceed to change state safely.
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $registry
+
 echo What version would you like to tag this release? e.g. v0.1.0:
 
 read version
@@ -13,7 +16,6 @@ git push origin --tags
 
 echo Pushing $version to $registry/$repository 
 docker build -t $registry/$repository:$version -f Dockerfile.sidecar --platform=linux/amd64 .
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin $registry
 docker push $registry/$repository:$version
 
 echo Released $version. View the release here: https://gallery.ecr.aws/g0r8j2n2/lekko/sidecar
