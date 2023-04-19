c512ea4c492a1064a55b36842978b84de5a1fcb1
Shubhit Singh <shubhit@lekko.dev>
2023-04-19 16:56:26 -0700

    clippy

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -3,10 +3,7 @@
 // This file re-exports an auto-generated include file by prost-gen-prost-crate but adds
 // clippy allows, which is incorrect b/c protobuf generated code doesn't implement Eq by
 // default since they want you to use protobuf's equality specifically.
+#[allow(clippy::derive_partial_eq_without_eq)]
 pub mod mod_cli;
 #[allow(clippy::derive_partial_eq_without_eq)]
-// mod mod_cli;
-// mod mod_sdk;
-// pub use mod_cli::*;
-// pub use mod_sdk::*;
 pub mod mod_sdk;
