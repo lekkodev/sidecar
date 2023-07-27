b47269739a0a55fe81d0410fdff1f47fbf430f85
Shubhit Singh <3323832+shubhitms@users.noreply.github.com>
2023-07-27 13:10:53 -0600

    Update release script to support multiple architectures (#53)

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-registry=public.ecr.aws/g0r8j2n2
+registry=public.ecr.aws/lekko
 repository=lekko/sidecar
 
 # First, check for credentials so we can later proceed to change state safely.
@@ -12,12 +12,34 @@ echo What version would you like to tag this release? e.g. v0.1.0:
 
 read version
 
+echo Releasing version $version
+
+for arch in amd64 arm64
+do
+    echo Building for architecture linux/$arch
+    docker build -t $registry/$repository:$version-$arch --build-arg SIDECAR_VERSION=$version --build-arg SIDECAR_GIT_COMMIT=$git_commit -f Dockerfile.sidecar --arch=linux/$arch .
+done
+
 echo Tagging version $version
 git tag -a $version -m $version -f
 git push origin --tags
 
-echo Pushing $version to $registry/$repository 
-docker build -t $registry/$repository:$version --build-arg SIDECAR_VERSION=$version --build-arg SIDECAR_GIT_COMMIT=$git_commit -f Dockerfile.sidecar --platform=linux/amd64 .
-docker push $registry/$repository:$version
+for arch in amd64 arm64
+do
+    echo Pushing $version-$arch to $registry/$repository
+    docker push $registry/$repository:$version-$arch
+done
 
-echo Released $version. View the release here: https://gallery.ecr.aws/g0r8j2n2/lekko/sidecar
+echo Creating manifest list for $version
+docker manifest create $registry/$repository:$version \
+    $registry/$repository:$version-amd64  \
+    $registry/$repository:$version-arm64
+
+for arch in amd64 arm64
+do
+    docker manifest annotate --arch $arch $registry/$repository:$version $registry/$repository:$version-$arch    
+done
+
+docker manifest push $registry/$repository:$version
+
+echo Released $version. View the release here: https://gallery.ecr.aws/lekko/lekko/sidecar
